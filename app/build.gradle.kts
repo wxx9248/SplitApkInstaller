d3923282bad48fc74c4ca669fbdcc197b6d1ccbd
wxx9248 <wxx9248@wxx9248.top>
2026-02-28 15:34:44 -0800

    bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "top.wxx9248.splitapkinstaller"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
